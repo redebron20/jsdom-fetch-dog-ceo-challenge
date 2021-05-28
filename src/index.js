baccaea8b3353493f858c5254b31da677f2427c3
redebron20 <redebron20@gmail.com>
2021-05-28 12:51:07 -0400

    commit tree c14cc51c46d88ebf4e8f1d677456f099473990a4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function(){
+    loadImages();
+});
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    fetch('imgUrl')
+        .then(resp => resp.json())
+        .then(results => {
+            results.message.forEach(image => addImage(image))
+          });
+}
+
+function renderImages(imgs) {
+
+}
